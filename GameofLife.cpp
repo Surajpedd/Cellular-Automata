23b8c8995eebc2c7fba621d33f28bde9c6aefd8d
Surajpedd <58389876+Surajpedd@users.noreply.github.com>
2020-12-23 19:38:07 +0530

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -3,42 +3,42 @@
 #include<algorithm>   //for copy()
 using namespace std;
 const int gridSize = 25;
-void printGrid(bool gridOne[gridSize+1][gridSize+1])
+Algorithm printGrid(bool gridOne[gridSize+1][gridSize+1])
 {
     for(int i=1;i<gridSize;i++)
     {
         for(int j=1;j<gridSize;j++)
         {
             if(gridOne[i][j])	
-                cout << " \u25A0 ";
+                Print " \u25A0 "  //print square if alive
             else
-                cout << " # ";
+                Print " # "   // print hash if dead
         }
-        cout<<endl;
+        Print newline
     }
 }
-int check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)                    //returns number of living cells adjacent to (a,b)
+Algorithm check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)                    //returns number of living cells adjacent to (a,b)
 {
-    int alive=0;
-    for(int i = -1; i < 2; i++)
+    Set alive = 0
+    for(int i = -1; i < 2; i++)                                                   
         for(int j = -1; j < 2; j++)
             if(!(i == 0 && j == 0) && gridTwo[a+i][b+j]==true)                  //checking all adjacent cells are alive 
-	     	    alive++;
+	     	    alive=alive+1
     return alive;
 }
-void determineState(bool gridOne[gridSize+1][gridSize+1])
+Algorithm determineState(bool gridOne[gridSize+1][gridSize+1])
 {
-    bool gridTwo[gridSize+1][gridSize+1]={};
-    copy(&gridOne[0][0], &gridOne[0][0]+(gridSize+1)*(gridSize+1),&gridTwo[0][0]);               //copy gridOne into gridTwo
-    int alive;
+    bool gridTwo[gridSize+1][gridSize+1]={}
+    copy(&gridOne[0][0], &gridOne[0][0]+(gridSize+1)*(gridSize+1),&gridTwo[0][0])               //copy gridOne into gridTwo
+    int alive
     for(int a = 1; a < gridSize; a++)
         for(int b = 1; b < gridSize; b++)
         {
-            alive=check(gridTwo,a,b);
+            alive=check(gridTwo,a,b)
             if(alive < 2 || alive > 3 && gridTwo[a][b]==true)           //if less than 2 neighbours alive or more than 3 then cell dies
-                gridOne[a][b] = false;
+                gridOne[a][b] = false
             else if(alive == 3 && gridTwo[a][b]==false)                 //if three adjacent cells are alive then cell is birthed
-                gridOne[a][b] = true;
+                gridOne[a][b] = true
         }
 }
 Algorithm main()
