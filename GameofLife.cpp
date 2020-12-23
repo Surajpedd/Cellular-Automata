f0b4aa04d7af0fef42b7dde397788f0ff359ab83
Surajpedd <58389876+Surajpedd@users.noreply.github.com>
2020-12-23 19:43:00 +0530

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -3,62 +3,64 @@
 #include<algorithm>   //for copy()
 using namespace std;
 const int gridSize = 25;
-Algorithm printGrid(bool gridOne[gridSize+1][gridSize+1])
+void printGrid(bool gridOne[gridSize+1][gridSize+1])
 {
     for(int i=1;i<gridSize;i++)
     {
         for(int j=1;j<gridSize;j++)
         {
             if(gridOne[i][j])	
-                Print " \u25A0 "  //print square if alive
+                cout << " \u25A0 ";
             else
-                Print " # "   // print hash if dead
+                cout << " # ";
         }
-        Print newline
+        cout<<endl;
     }
 }
-Algorithm check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)                    //returns number of living cells adjacent to (a,b)
+int check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)                    //returns number of living cells adjacent to (a,b)
 {
-    Set alive = 0
-    for(int i = -1; i < 2; i++)                                                   
+    int alive=0;
+    for(int i = -1; i < 2; i++)
         for(int j = -1; j < 2; j++)
             if(!(i == 0 && j == 0) && gridTwo[a+i][b+j]==true)                  //checking all adjacent cells are alive 
-	     	    alive=alive+1
-    return alive
+	     	    alive++;
+    return alive;
 }
-Algorithm determineState(bool gridOne[gridSize+1][gridSize+1])
+void determineState(bool gridOne[gridSize+1][gridSize+1])
 {
-    bool gridTwo[gridSize+1][gridSize+1]={}
-    copy(&gridOne[0][0], &gridOne[0][0]+(gridSize+1)*(gridSize+1),&gridTwo[0][0])               //copy gridOne into gridTwo
-    int alive
+    bool gridTwo[gridSize+1][gridSize+1]={};
+    copy(&gridOne[0][0], &gridOne[0][0]+(gridSize+1)*(gridSize+1),&gridTwo[0][0]);               //copy gridOne into gridTwo
+    int alive;
     for(int a = 1; a < gridSize; a++)
         for(int b = 1; b < gridSize; b++)
         {
-            alive=check(gridTwo,a,b)
+            alive=check(gridTwo,a,b);
             if(alive < 2 || alive > 3 && gridTwo[a][b]==true)           //if less than 2 neighbours alive or more than 3 then cell dies
-                gridOne[a][b] = false
-            else if(alive == 3 && gridTwo[a][b]==false)                 //if three adjacent cells are alive then cell is born
-                gridOne[a][b] = true
+                gridOne[a][b] = false;
+            else if(alive == 3 && gridTwo[a][b]==false)                 //if three adjacent cells are alive then cell is birthed
+                gridOne[a][b] = true;
         }
 }
-Algorithm main()
+int main()
 {
-    bool gridOne[gridSize+1][gridSize+1]={}                             //gridSize=25 (any value can be chosen)
-    int x,y,nc
-    Read nc
+    bool gridOne[gridSize+1][gridSize+1]={};                             //gridSize=25 (any value can be chosen)
+    int x,y,nc;
+    cin >> nc;
     for(int i=0;i<nc;i++)
     {
-	Read x and y coordinates 
-	gridOne[x][y] = true
-	printGrid(gridOne)
+	cout <<"Enter the coordinates of cell " << i+1 << " : "<<endl;
+	cin>>x>>y;
+	gridOne[x][y] = true;
+	printGrid(gridOne);
     }
-    Set ch='Y'
+    char ch='Y';
     for(nc=1;ch=='Y';nc++)                                              //for loop to generate next state until break
     {
-      	system("clear")
-        printGrid(gridOne)
-        Print "Continue? [Y/N]"
-        Read ch
+      	system("clear");
+        cout<<"State "<<nc<<endl;
+        printGrid(gridOne);
+        cout<<"Continue? [Y/N]";
+        cin>>ch;
         determineState(gridOne);
     }
 }
