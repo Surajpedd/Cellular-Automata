7e20df9fa6659576a9c6682e549ce11be23e6eed
Surajpedd <58389876+Surajpedd@users.noreply.github.com>
2020-12-23 12:00:26 +0530

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -17,12 +17,12 @@ void printGrid(bool gridOne[gridSize+1][gridSize+1])
         cout<<endl;
     }
 }
-int check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)
+int check(bool gridTwo[gridSize+1][gridSize+1],int a,int b)                    //returns number of living cells adjacent to (a,b)
 {
     int alive=0;
     for(int i = -1; i < 2; i++)
         for(int j = -1; j < 2; j++)
-            if(!(i == 0 && j == 0) && gridTwo[a+i][b+j]==true)                  //checking all adjacent cells are alive
+            if(!(i == 0 && j == 0) && gridTwo[a+i][b+j]==true)                  //checking all adjacent cells are alive 
 	     	    alive++;
     return alive;
 }
@@ -35,15 +35,15 @@ void determineState(bool gridOne[gridSize+1][gridSize+1])
         for(int b = 1; b < gridSize; b++)
         {
             alive=check(gridTwo,a,b);
-            if(alive < 2 || alive > 3)               //if less than 2 neighbours alive and more than 3 then cell dies
+            if(alive < 2 || alive > 3 && gridTwo[a][b]==true)           //if less than 2 neighbours alive and more than 3 then cell dies
                 gridOne[a][b] = false;
-            else if(alive == 3)                      //if three adjacent cells are alive then cell is birthed
+            else if(alive == 3 && gridTwo[a][b]==false)                 //if three adjacent cells are alive then cell is birthed
                 gridOne[a][b] = true;
         }
 }
 int main()
 {
-    bool gridOne[gridSize+1][gridSize+1]={};
+    bool gridOne[gridSize+1][gridSize+1]={};                             //gridSize=25 (any value can be chosen)
     char ch='Y';
     int x,y,nc;
     cin >> nc;
@@ -54,9 +54,9 @@ int main()
 	gridOne[x][y] = true;
 	printGrid(gridOne);
     }
-    for(nc=1;ch=='Y';nc++)
+    for(nc=1;ch=='Y';nc++)                                              //for loop to generate next state until break
     {
-      	 system("clear");                   //for windows use clrscr()
+      	system("clear");
         cout<<"State "<<nc<<endl;
         printGrid(gridOne);
         cout<<"Continue? [Y/N]";
@@ -67,4 +67,5 @@ int main()
 // loaf- (5 5),(6,5),(5,6),(7,5),(5,7)  static
 // boat - (4,5),(5,4),(6,5),(5,6),(7,7) static
 //beacon - (1,1),(2,1),(1,2),(4,4),(3,4),(4,3) oscillating
-// glider - (6,5),(7,6),(5,7),(6,7),(7,7) 
+// glider - (6,5),(7,6),(5,7),(6,7),(7,7)  moves across grid (time period of 4)
+// blinker - (5,5),(6,5),(7,5) oscillating (time period 2)
